ae4b24bbf47d35e30dd5f5de9b1f571fbe49435c
Joel Leimbacher <joel.leimbacher@evosys.ch>
2026-05-06 14:58:51 +0200

    Add AareTempBar cask v0.9.0

---
+++ Casks/aare-temp-menubar.rb
@@ -0,0 +1,9 @@
+cask "aare-temp-menubar" do
+  version "0.9.0"
+  sha256 :no_check
+  url "https://github.com/jl115/aare-temp-menubar/releases/download/v#{version}/AareTempBar-macos.zip"
+  name "AareTempBar"
+  desc "Live Aare river temperature in your macOS menubar"
+  homepage "https://github.com/jl115/aare-temp-menubar"
+  app "AareTempBar.app"
+end
