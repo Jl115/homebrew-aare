95d7b9a21f6bcfface990a6ac8e1c638011d6da0
Joel Leimbacher <joel.leimbacher@evosys.ch>
2026-05-06 15:09:24 +0200

    releas: 0.9.1

--- Casks/aare-temp-menubar.rb
+++ Casks/aare-temp-menubar.rb
@@ -1,5 +1,5 @@
 cask "aare-temp-menubar" do
-  version "0.9.0"
+  version "0.9.1"
   sha256 :no_check
   url "https://github.com/jl115/aare-temp-menubar/releases/download/v#{version}/AareTempBar-macos.zip"
   name "AareTempBar"
